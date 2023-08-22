41bc9ebd5ec1b2585faee431e151c72c353d9d9f
JosthinAyon <josthinayon@gmail.com>
2023-08-21 21:37:28 -0500

    A seguir trabajando en errores

---
+++ CooperativaDeBuses/Controllers/TicketsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CooperativaDeBuses.Models;
+using CooperativaDeBuses.Models.Repositories.TicketRepository;
+using AutoMapper;
+
+
+namespace CooperativaDeBuses.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly ITicketRepository _ticketRepository;
+        private readonly IMapper _mapper;
+
+        public TicketsController(ITicketRepository ticketRepository, IMapper mapper)
+        {
+            _ticketRepository = ticketRepository;
+            _mapper = mapper;
+        }
+
+       [HttpGet]
+        public async Task<ActionResult<IEnumerable<Ticket>>> GetTickets()
+        {
+            try
+            {
+                var tickets = await _ticketRepository.GetListTicket();
+                return Ok(tickets);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("usuario/{id}")]
+        public async Task<ActionResult<IEnumerable<Ticket>>> GetTicketsByUserId()
+        {
+            try
+            {
+                var tickets = await _ticketRepository.GetListTicket();
+                return Ok(tickets);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Ticket>> GetTicket(int id)
+        {
+            try
+            {
+                var ticket = await _ticketRepository.GetTicket(id);
+                if (ticket == null)
+                {
+                    return NotFound();
+                }
+                return ticket;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Ticket>> PostTicket([FromBody] Ticket ticket)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                await _ticketRepository.AddTicket(ticket);
+
+                return Ok(new { message = "Ticket guardado exitosamente" });
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Ticket>> PutTicket(int id, [FromBody] TicketDto ticketDto)
+        {
+            try
+            {
+                var ticket = _mapper.Map<Ticket>(ticketDto);
+                ticket.Id = id;
+                var ticketItem = await _ticketRepository.GetTicket(id);
+
+                if (ticketItem == null)
+                {
+                    return NotFound();
+                }
+
+                Ticket ticketA = await _ticketRepository.UpdateTicket(ticket);
+
+                return Ok(ticketA);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("eliminar/{id}")]
+        public async Task<ActionResult<Ticket>> DeleteTicket(int id)
+        {
+            try
+            {
+                await _ticketRepository.DeleteTicket(id);
+
+                return Ok(new { message = $"Ticket con id {id} eliminado" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ CooperativaDeBuses/Controllers/UsuariosController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CooperativaDeBuses.Models;
+using AutoMapper;
+
+using CooperativaDeBuses.Models.Repositories.UsuarioRepository;
+
+namespace CooperativaDeBuses.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly IUsuarioRepository _usuarioRepository;
+        private readonly IMapper _mapper;
+
+        public UsuariosController(IUsuarioRepository usuarioRepository, IMapper mapper)
+        {
+            _usuarioRepository = usuarioRepository;
+            _mapper = mapper;
+        }
+
+       [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
+        {
+            try
+            {
+                var Usuarios = await _usuarioRepository.GetListUsuario();
+                return Ok(Usuarios);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuario>> GetUsuario(int id)
+        {
+            try
+            {
+                var Usuario = await _usuarioRepository.GetUsuario(id);
+                if (Usuario == null)
+                {
+                    return NotFound();
+                }
+                return Usuario;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("ci/{id}")]
+        public async Task<ActionResult<Usuario>> GetUserByCi(string id)
+        {
+            try
+            {
+                var Usuario = await _usuarioRepository.GetByUserCi(id);
+                if (Usuario == null)
+                {
+                    return NotFound(new {message = "No se encontro un usuario con esa cedula"});
+                }
+                return Usuario;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> PostUsuario([FromBody] Usuario Usuario)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                Usuario UsuarioGuardado = await _usuarioRepository.AddUsuario(Usuario);
+
+                return Ok("Usuario guardado exitosamente");
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Usuario>> PutUsuario(int id, [FromBody] UsuarioDto UsuarioDto)
+        {
+            try
+            {
+                var Usuario = _mapper.Map<Usuario>(UsuarioDto);
+                Usuario.Id = id;
+                var UsuarioItem = await _usuarioRepository.GetUsuario(id);
+
+                if (UsuarioItem == null)
+                {
+                    return NotFound();
+                }
+
+                Usuario UsuarioA = await _usuarioRepository.UpdateUsuario(Usuario);
+
+                return Ok(UsuarioA);
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut("eliminar/{id}")]
+        public async Task<ActionResult<Usuario>> DeleteUsuario(int id)
+        {
+            try
+            {
+                await _usuarioRepository.DeleteUsuario(id);
+
+                return Ok(new { message = $"Usuario con id {id} eliminado" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
+    
